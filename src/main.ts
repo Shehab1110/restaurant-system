866db6fad8a044c40118209dfc956aa04bcf3165
Shehab Ashraf <shehabashrafh1110@gmail.com>
2024-06-07 02:47:01 +0300

    change config approach

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AllExceptionsFilter } from './errors/all-exceptions-filter';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config({ path: './config.env' });
+  const envFilePath: string =
+    process.env.NODE_ENV === 'production'
+      ? './src/envs/production.env'
+      : './src/envs/development.env';
+  dotenv.config({ path: envFilePath });
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
