201587ba1881e1b0a99b4cf89826c19ab44ee717
Shehab Ashraf <shehabashrafh1110@gmail.com>
2024-06-06 20:07:46 +0300

    add response interceptor

--- src/interceptors/sucess-response.interceptor.ts
+++ src/interceptors/sucess-response.interceptor.ts
@@ -6,18 +6,29 @@ import {
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 
+interface SuccessResponse<T> {
+  isSuccess: boolean;
+  message: string;
+  data: T;
+  errorCode: null;
+  errors: any[];
+  count?: number;
+}
+
 @Injectable()
-export class SucessResponseInterceptor implements NestInterceptor {
+export class SucessResponseInterceptor<T> implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      map((data) => {
-        return {
+      map((data: T) => {
+        const responseObject: SuccessResponse<T> = {
           isSuccess: true,
           message: 'success',
           data,
           errorCode: null,
           errors: [],
         };
+        if (Array.isArray(data)) responseObject.count = data.length;
+        return responseObject;
       }),
     );
   }
